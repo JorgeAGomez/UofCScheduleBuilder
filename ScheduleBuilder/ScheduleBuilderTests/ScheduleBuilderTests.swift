6e4031340b4fc8ae659132a1cf929de53d767718
Tiffany <tiffany@Circe.local>
2015-11-11 21:16:08 -0700

    add files

---
+++ ScheduleBuilder/ScheduleBuilderTests/ScheduleBuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScheduleBuilderTests.swift
+//  ScheduleBuilderTests
+//
+//  Created by Sasha Ivanov on 2015-10-30.
+//  Copyright © 2015 Alexander Ivanov. All rights reserved.
+//
+
+import XCTest
+@testable import ScheduleBuilder
+
+class ScheduleBuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

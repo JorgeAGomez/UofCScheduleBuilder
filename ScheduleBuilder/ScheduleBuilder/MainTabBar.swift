d721e44bb8c4a7ee0c8f5f5c7cb933ea55fbf9b2
Sasha Ivanov <bamboostickk@gmail.com>
2015-12-01 13:47:17 -0700

    Added Tab icons Fixed up story board

---
+++ ScheduleBuilder/ScheduleBuilder/MainTabBar.swift
@@ -0,0 +1,22 @@
+//
+//  MainTabBar.swift
+//  ScheduleBuilder
+//
+//  Created by Sasha Ivanov on 2015-12-01.
+//  Copyright © 2015 Alexander Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBar: UITabBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
--- ScheduleBuilder/ScheduleBuilder/ViewController.swift
+++ ScheduleBuilder/ScheduleBuilder/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Change the tab bar to the official U of C Color
+        MainTabBar.appearance().tintColor = UIColor(hue: 355/360, saturation: 0.92, brightness: 0.87, alpha: 1)
+        
     }
 
     override func didReceiveMemoryWarning() {

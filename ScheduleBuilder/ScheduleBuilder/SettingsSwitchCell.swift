bfc611d56bc9e9e04527260b99fee5bd2128be7e
Sasha Ivanov <bamboostickk@gmail.com>
2016-04-11 17:09:26 -0600

    Settings is here!

---
+++ ScheduleBuilder/ScheduleBuilder/SettingsSwitchCell.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsSwitchCell.swift
+//  ScheduleBuilder
+//
+//  Created by Sasha Ivanov on 2016-04-11.
+//  Copyright © 2016 Alexander Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class SettingsSwitchCell: UITableViewCell {
+
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

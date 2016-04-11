d8d0667df160b49c4a81c2d87119fabee21e46ac
Jorge <jorge_gomez12@hotmail.com>
2016-04-11 15:54:08 -0600

    Added PagingMenuController library

--- ScheduleBuilder/Pods/Target Support Files/Pods-ScheduleBuilder/Pods-ScheduleBuilder-frameworks.sh
+++ ScheduleBuilder/Pods/Target Support Files/Pods-ScheduleBuilder/Pods-ScheduleBuilder-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-ScheduleBuilder/PagingMenuController.framework"
   install_framework "Pods-ScheduleBuilder/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-ScheduleBuilder/PagingMenuController.framework"
   install_framework "Pods-ScheduleBuilder/SwiftyJSON.framework"
 fi

f227c66c4a0db71a55f681188a443e1cbce2ae75
Jorge <jorge_gomez12@hotmail.com>
2016-04-11 16:35:41 -0600

    added new view controllers. feature tab works. just need to populate table views

--- ScheduleBuilder/ScheduleBuilder/FeatureViewController.swift
+++ ScheduleBuilder/ScheduleBuilder/FeatureViewController.swift
@@ -20,6 +20,25 @@ class FeatureViewController: UIViewController {
       options.selectedFont = UIFont(name: "American Typewriter", size: 20)!
     
       let viewController1 = self.storyboard?.instantiateViewControllerWithIdentifier("Feature1") as! Feature1ViewController
+      let viewController2 = self.storyboard?.instantiateViewControllerWithIdentifier("Feature2") as! Feature2ViewController
+      let viewController3 = self.storyboard?.instantiateViewControllerWithIdentifier("Feature3") as! Feature3ViewController
+      let viewController4 = self.storyboard?.instantiateViewControllerWithIdentifier("Feature4") as! Feature4ViewController
+      let viewController5 = self.storyboard?.instantiateViewControllerWithIdentifier("Feature5") as! Feature5ViewController
+    
+      viewController1.title = "Design"
+      viewController2.title = "Popular"
+      viewController3.title = "No Pre-Req"
+      viewController4.title = "Coding"
+      viewController5.title = "No final"
+    
+      let viewControllers = [viewController1, viewController2, viewController3, viewController4, viewController5]
+    
+      options.menuHeight = 35
+      options.menuItemMode = .Underline(height: 2.0, color: UIColor.redColor(), horizontalPadding: 0.1, verticalPadding: 0.1)
+    
+      let pagingMenuController = self.childViewControllers.first as! PagingMenuController
+      options.menuDisplayMode = .Infinite(widthMode: .Flexible)
+      pagingMenuController.setup(viewControllers: viewControllers, options: options)
     
   }
 

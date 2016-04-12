4c0218e1f4cc90bb053766a70145c666350a0481
Sasha Ivanov <bamboostickk@gmail.com>
2016-04-12 01:41:59 -0600

    Course Page Redesign (Buggy but better)

--- ScheduleBuilder/ScheduleBuilder/CoursePageView.swift
+++ ScheduleBuilder/ScheduleBuilder/CoursePageView.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class CoursePageView: UIView {
 
-    /*
+    let backgroundBoxColour = UIColor(hue: 0, saturation: 0.1, brightness: 1.00, alpha: 1.00)
+    
+    var isOpen = false
+    
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-        // Drawing code
+        
+        // Create Top Rounded Background Box
+        let backgroundBox = UIBezierPath(roundedRect: CGRectMake(0, 0, self.frame.width, 30), byRoundingCorners: [.TopLeft, .TopRight], cornerRadii: CGSizeMake(8, 8))
+        
+        // Set Background Box Color & Draw
+        backgroundBoxColour.setFill()
+        backgroundBox.fill()
+        
+        
+        // Set Schedule Title
+        var titleLabel = UILabel(frame: CGRectMake(10, 5, self.frame.width - 15, 30))
+        titleLabel.textAlignment = NSTextAlignment.Left
+        titleLabel.font = titleLabel.font.fontWithSize(20)
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.text = "New Schedule"
+        self.addSubview(titleLabel)
+        
     }
-    */
+    
 
 }
---
+++ ScheduleBuilder/ScheduleBuilder/OfferingView.swift
@@ -0,0 +1,170 @@
+//
+//  OfferingViewTop.swift
+//  ScheduleBuilder
+//
+//  Created by Sasha Ivanov on 2016-04-11.
+//  Copyright © 2016 Alexander Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class OfferingView: UIView {
+
+    
+    let backgroundBoxColour = UIColor(hue: 0, saturation: 0, brightness: 00.95, alpha: 1.00)
+
+    var lecture:Lecture!
+    
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+
+        self.backgroundColor = UIColor.whiteColor()
+        let secHeight:CGFloat = 30
+        let spaceHeight:CGFloat = 10
+        
+        // Create Top Rounded Background Box
+        let topBox = UIBezierPath(roundedRect: CGRectMake(0, 0, self.frame.width, secHeight), byRoundingCorners: [.TopLeft, .TopRight], cornerRadii: CGSizeMake(8, 8))
+
+        // Set Background Box Color & Draw
+        backgroundBoxColour.setFill()
+        topBox.fill()
+        
+        // Create Lecture Text
+        let titleLabel = UILabel(frame: CGRectMake(10, 0, self.frame.width - 15, secHeight))
+        titleLabel.textAlignment = NSTextAlignment.Left
+        titleLabel.font = UIFont.systemFontOfSize(14, weight: UIFontWeightRegular)
+
+        var titleText = "Lecture"
+        
+        if(lecture?.number != nil){
+            titleText += " " + String(lecture.number) + "  "
+        }
+        
+        titleLabel.text = titleText
+        titleLabel.textColor = UIColor.blackColor()
+        self.addSubview(titleLabel)
+        
+        // Create Lecture Time Text
+        let timeLabel = UILabel(frame: CGRectMake(10, titleLabel.bounds.height/1.8, self.frame.width - 15, secHeight))
+        timeLabel.textAlignment = NSTextAlignment.Left
+        timeLabel.font = UIFont.systemFontOfSize(14, weight: UIFontWeightLight)
+        
+        var timeText = ""
+        
+        if(lecture?.time != nil){
+            
+            for t in lecture.time {
+                timeText += t.day + " "
+                
+            }
+
+            timeText += (lecture.time.first?.fromTimeText)! + " - "
+            timeText += (lecture.time.first?.toTimeText)!
+        }
+        
+        timeLabel.text = timeText
+        timeLabel.textColor = UIColor.blackColor()
+        self.addSubview(timeLabel)
+        
+        // Create Mid Background Box
+        
+        var subSections = 0
+        
+        if(lecture?.tutorials != nil){
+            subSections += (lecture.tutorials?.count)!
+        }
+        
+        if(lecture?.labs != nil){
+            subSections += (lecture.labs?.count)!
+        }
+        
+        let midBox = UIBezierPath(rect: CGRectMake(0, topBox.bounds.height, self.frame.width, secHeight*CGFloat(subSections)))
+        backgroundBoxColour.setFill()
+        midBox.fill()
+        
+        // Create Tutorial & Lab Text
+        
+        if(lecture?.tutorials != nil){
+            
+            var tutText = ""
+            var tutTime = ""
+            
+            for tut in lecture.tutorials!{
+                tutText = "Tutorial " + String(tut.number)
+                
+                for t in tut.time {
+                    tutTime += t.day + " "
+                }
+                
+                if(tut.time.first != nil){
+                    tutTime += (tut.time.first?.fromTimeText)! + " - "
+                    tutTime += (tut.time.first?.toTimeText)!
+                }
+                
+                let tutLabel = UILabel(frame: CGRectMake(10, CGFloat(tut.number)*(secHeight+spaceHeight) , self.frame.width - 15, secHeight))
+                tutLabel.textAlignment = NSTextAlignment.Left
+                tutLabel.font = UIFont.systemFontOfSize(14, weight: UIFontWeightRegular)
+                tutLabel.text = tutText
+                tutLabel.textColor = UIColor.blackColor()
+                self.addSubview(tutLabel)
+                
+                let tutTimeLabel = UILabel(frame: CGRectMake(10, tutLabel.bounds.height/1.8 + CGFloat(tut.number)*(secHeight+spaceHeight), self.frame.width - 15, secHeight))
+                tutTimeLabel.textAlignment = NSTextAlignment.Left
+                tutTimeLabel.font = UIFont.systemFontOfSize(14, weight: UIFontWeightLight)
+                tutTimeLabel.text = tutTime
+                tutTimeLabel.textColor = UIColor.blackColor()
+                self.addSubview(tutTimeLabel)
+                
+                
+            }
+        }
+        
+        if(lecture?.labs != nil){
+            
+            var labText = ""
+            var labTime = ""
+            
+            for lab in lecture.labs!{
+                labText = "Tutorial " + String(lab.number)
+                
+                for t in lab.time {
+                    labTime += t.day + " "
+                }
+                
+                if(lab.time.first != nil){
+                    labTime += (lab.time.first?.fromTimeText)! + " - "
+                    labTime += (lab.time.first?.toTimeText)!
+                }
+                
+                let labLabel = UILabel(frame: CGRectMake(10, CGFloat(lab.number)*(secHeight+spaceHeight) , self.frame.width - 15, secHeight))
+                labLabel.textAlignment = NSTextAlignment.Left
+                labLabel.font = UIFont.systemFontOfSize(14, weight: UIFontWeightRegular)
+                labLabel.text = labText
+                labLabel.textColor = UIColor.blackColor()
+                self.addSubview(labLabel)
+                
+                let labTimeLabel = UILabel(frame: CGRectMake(10, labLabel.bounds.height/1.8 + CGFloat(lab.number)*(secHeight+spaceHeight), self.frame.width - 15, secHeight))
+                labTimeLabel.textAlignment = NSTextAlignment.Left
+                labTimeLabel.font = UIFont.systemFontOfSize(14, weight: UIFontWeightLight)
+                labTimeLabel.text = labTime
+                labTimeLabel.textColor = UIColor.blackColor()
+                self.addSubview(labTimeLabel)
+            }
+        }
+
+        
+        
+        
+        
+        // Create Bottom Background Box
+
+        let btmBox = UIBezierPath(roundedRect: CGRectMake(0, topBox.bounds.height + midBox.bounds.height, self.frame.width, secHeight), byRoundingCorners: [.BottomLeft, .BottomRight], cornerRadii: CGSizeMake(8, 8))
+        backgroundBoxColour.setFill()
+        btmBox.fill()
+        
+    }
+    
+
+}

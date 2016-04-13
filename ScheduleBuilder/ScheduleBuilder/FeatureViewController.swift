60f6d905e552cb15c2d0547f44c6fa3ec6ac2d93
Sasha Ivanov <bamboostickk@gmail.com>
2016-04-13 12:33:24 -0600

    featured tab naming bug fix

--- ScheduleBuilder/ScheduleBuilder/FeatureViewController.swift
+++ ScheduleBuilder/ScheduleBuilder/FeatureViewController.swift
@@ -14,7 +14,7 @@ class FeatureViewController: UIViewController {
   
   override func viewDidLoad() {
       super.viewDidLoad()
-      self.title = "Featured Courses"
+      self.title = "Featured"
     
       let options = PagingMenuOptions()
       options.selectedFont = UIFont(name: "American Typewriter", size: 20)!

571c16de9693a396a382f864d046a10035cf8e35
nryzhenk <ryzhenkovn@gmail.com>
2016-04-27 11:26:17 -0600

    cleaned up ScheduleViewController

--- ScheduleBuilder/ScheduleBuilder/ScheduleBuilderViewController.swift
+++ ScheduleBuilder/ScheduleBuilder/ScheduleBuilderViewController.swift
@@ -28,10 +28,7 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
     // flattened favorited courses. for use in cells
     var listOfFlattenedCourses: [[CourseCellData]] = []  // <--- build cells off of this thang
     
-    
-    
-    //SASHALAND
-    //*********************************
+
     var totalOfferings: [Int]   = []
     var tutOfferings:   [Int]   = []
     var labOfferings:   [Int]   = []
@@ -58,8 +55,6 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
     var expandedIndex = -1
     // Accordion
     
-    //*********************************
-    //SASHALAND
     
     
     override func viewDidLoad() {
@@ -82,10 +77,7 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
         
         let SaveButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Save, target: self, action: "saveSchedule")
         navigationItem.rightBarButtonItem = SaveButton
-        
 
-        
-        // Do any additional setup after loading the view.
     }
     
     
@@ -176,10 +168,7 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
             cell.offeringTime.text = text
         }
         
-        
-        // determine whether to grey out this cell
-        var a = indexPath.section
-        var b = indexPath.row
+        //greying out of the cells
         if (cell.chosen != nil)
         {
             if(cell.chosen! == true)
@@ -194,7 +183,6 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
         {
             cell.userInteractionEnabled = false
             cell.backgroundColor = UIColor.darkGrayColor()
-            //cell.textLabel!.textColor = UIColor.redColor()
         }
         else
         {
@@ -257,7 +245,7 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
         let indexPath : NSIndexPath = NSIndexPath(forRow: 0, inSection:(recognizer.view?.tag as Int!)!)
         if (indexPath.row == 0) {
             
-            expandedIndex = -1                          // might cause problems?
+            expandedIndex = -1      // might cause problems?
             //reload specific section animated
             let range = NSMakeRange(indexPath.section, 1)
             let sectionToReload = NSIndexSet(indexesInRange: range)
@@ -270,17 +258,12 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
     {
         if let cell = tableView.cellForRowAtIndexPath(indexPath) as? OfferingTableViewCell{
-            var a = indexPath.section
-            var b = indexPath.row
+
             // If cell is already checked, uncheck it and update greyedout cells
             if cell.accessoryType == .Checkmark
             {
-                // Uncheck cell
-                //cell.accessoryType = .None
-                
                 // update the list of cells to make sure only the correct cells are greyed out
                 ungreyMeLikeOneOfYourFrenchGirls(indexPath.section, lectureNum: cell.lectureNum, type: cell.type, typeNum: cell.num)
-                
                 replacePeriodicWithCellData_REMOVE(indexPath.section, type: cell.type)
                 
             }
@@ -405,9 +388,8 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
                     else // tutorial or lab or both were selected. If tut was selected we don't ungrey tuts, same with labs.
                     {
                         // technically speakng there is no need for ungreying the already ungreyed cells. This is strictly as a failsafe
-                        // frankly I wrote it without thinking but it works and doesnt' really add to time complexity so there.
                         
-                        // if no tut ws selected
+                        // if no tut was selected
                         if !tutSelected
                         {
                             if ccd.type == "tutorial"
@@ -517,7 +499,7 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
         }
         else
         {
-            // something is very, very wrong
+            // something is very, very wrong. Logically this can't actually happen
         }
         // remove new periodic from the schedule, check if it fits with the rest if it does draw
         // if it doesn't don't
@@ -538,18 +520,15 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
         // grab appropriate course from schedule. Maybe be partially completed at any point time
         var p = schedule_new[indexCourse]
         if type == "lecture" {
-            // p.times[0] = [time_L]
             p.times[0] = []
         }
         else if type == "tutorial"
         {
-            //p.times[1] = [time_t]
             p.times[1] = []
             
         }
         else if type == "lab"
         {
-            //p.times[2] = [time_l]
             p.times[2] = []
         }
         else{
@@ -567,13 +546,7 @@ class ScheduleBuilderViewController: UIViewController, NSFetchedResultsControlle
         }
         
     }
-    
-    // often time cells need to be autoc-clicked/chosen/marked. first when we load view with existing schedule
-    // and after every time we make a selection or deselection
-    private func autochooser()
-    {
-    
-    }
+
     // If the schedule is empty we hill populate it ourselves with phony data as a placeholder so that we can print shit
     // notice the times and days.
     // we generate one phony Periodic_new for each favorited course
